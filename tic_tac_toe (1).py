7a94bc1572a74dd47f51d305dacf4ff88c7a888c
linguisticity <153077731+linguisticity@users.noreply.github.com>
2023-12-08 17:03:51 +0530

    Add files via upload

--- tic_tac_toe (1).py
+++ tic_tac_toe (1).py
@@ -33,10 +33,14 @@
 ## modules for this task.								    ##
 ##############################################################
 import turtle
+import threading
 from sheet_dot_best_manager import sheet_manager
 ##############################################################
 
-
+connect=sheet_manager('https://sheet.best/api/sheets/49dbdd14-2dce-4a70-be43-7adcad372027')
+connected=False
+playing_as=''
+data=[]
 # Initialize the screen
 screen = turtle.Screen()
 screen.setup(800, 800)
@@ -45,29 +49,107 @@ screen.setworldcoordinates(-5, -5, 5, 5)
 screen.bgcolor('light gray')
 screen.tracer(0, 0)
 turtle.hideturtle()
+b = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-play = turtle.Turtle()
+def atoi(a, b):
+    if a == 'o' and b == False:
+        return 2
+    elif a == 'o' and b == True:
+        return 1
+    elif a=='x' and b==False:
+        return 1
+    elif a=='x' and b==True:
+        return 2
+    return 1
+
+
+
+def clear():
+    n=len(data)
+    print(n, 'clear() called')
+    for i in range(2, n+2):
+        connect.delete_data(i)
+
+def get_turn():
+    if data!=None and data!=[]:
+        turno=data[0]['turn']
+        if turno==None:
+            set_turn('x')
+            return 'x'
+        return turno
+    else:
+        set_turn('x')
+    return 'x'
+
+def set_turn(t):
+    global turn
+    connect.update_fields(2,{'turn':t})
+    turn=t
+
+
+def set_player(x,y):
+    connect.update_fields(2,{x: y})
+        
+    
+    
+def inito(x,y):
+    print('inito called')
+    global data
+    global connected
+    global playing_as
+    
+    print(data)
+    if data!=None:
+        if data==[]:
+            print(data)
+            playing_as='x'
+            connect.send_data([{'turn': t}])
+            connect.send_data([{x: y}])
+            playb.setx(5466)
+            connected=True
+        elif data[0]['b'] == None:
+            set_player('b','x')
+            playing_as='x'
+            set_turn('x')
+            playb.setx(3627)
+            connected=True
+        elif data[0]['a'] != None:
+            set_player('b','o')
+            playing_as='o'
+            set_turn('o')
+            playb.setx(5466)
+            connected=True
+        elif data[0]['b'] != None:
+            set_player('a','o')
+            set_turn('o')
+            playing_as='o'
+            playb.setx(5466)
+            connected=True
+        if data!=[]:
+            if data[0]['b'] != None and data[0]['a'] != None:
+                clear()
+    else:
+        print("Data: null")
+    
+
+playb = turtle.Turtle()
 turtle.register_shape('play.gif')
-play.shape('play.gif')
-play.penup()
-play.goto(150, 150)
-play.onclick(init())
+playb.shape('play.gif')
+playb.penup()
+playb.goto(0, 0)
+playb.onclick(inito)
+screen.update()
 
 
 # Ask for player names
 player1_name = screen.textinput("Player 1 Name", "Enter Player 1's name:")
 player2_name = screen.textinput("Player 2 Name", "Enter Player 2's name:")
 
-data=None
+moves=0
 
-connect=sheet_manager('https://sheet.best/api/sheets/49dbdd14-2dce-4a70-be43-7adcad372027')
 
-def init():
-    while True:
-        data=connect.get_all_data()
-    if data[0]['a'] == "":
-        data[0]['a']=='X'
-        
+
+    
 
 # Function to draw the board of Tic Tac Toe
 def draw_board():
@@ -127,12 +209,32 @@ def draw_piece(i, j, p):
 
 # Function to decide when to call draw_x(x,y) and draw_circle(x,y)
 def draw(b):
-    
-    #draw_board()
+    print('called draw()')
+    draw_board()
     for i in range(3):
         for j in range(3):
             draw_piece(i, j, b[i][j])
     screen.update()
+    
+def update(data):
+    for k in data:
+        if k['move']!=playing_as and k['move'] != None:
+            print(k['row'], k['column'])
+            row=int(k['row'])
+            col=int(k['column'])
+            b[row][col] = atoi(playing_as, True)
+    
+    
+    
+def con():
+    global data, turn
+    threading.Timer(1.0, con).start()
+    datai=connect.get_all_data()
+    data=datai['Data']
+    update(data)
+    turn=get_turn()
+
+con()
 
 # Function to decide if the game has reached a terminal condition or not
 def gameover(b):
@@ -150,15 +252,19 @@ def gameover(b):
             p += (1 if b[i][j] > 0 else 0)
     if p == 9: return 3
     else: return 0
-moves=0
+    
+
+
+
+
 # Function  to detect click on the screen
 def play(x, y):
-    
+    global moves, playing_as, data
+    global connected
     global turn
     global b
     if gameover(b) > 0:
-        for i in range(moves+1):
-            connect.delete_data(i)
+        
         winner = ""
         if gameover(b) == 1:
             winner = player1_name
@@ -172,21 +278,40 @@ def play(x, y):
             screen.bye()  # Close the game window
             return
 
-    i = 3 - int(y + 5) // 2
-    j = int(x + 5) // 2 - 1
-    if i > 2 or j > 2 or i < 0 or j < 0 or b[i][j] != 0:
-        return
     
-    data=[{'move':turn,'row':i,'column':j}]
-
-    if turn == 'x':
-        b[i][j], turn = 1, 'o'
+    
+    
+    
+    datai=[]
+    
+    if turn == playing_as and connected==True and draw!=[]:
+        
+        i = 3 - int(y + 5) // 2
+        j = int(x + 5) // 2 - 1
+        if i > 2 or j > 2 or i < 0 or j < 0 or b[i][j] != 0:
+            return
+        
+        datai=[{'move':turn,'row':i,'column':j}]
+        
+        b[i][j] = atoi(playing_as, False)
+        print('turn', turn)
+        print('pas', playing_as)
+        draw(b)
+        
         moves=+1
-    else:
-        b[i][j], turn = 2, 'x'
-        moves=+1
-    draw(b)
-    connect.send_data(data)
+        if playing_as == 'x':
+            turn='o'
+            set_turn('o')
+        elif playing_as == 'o':
+            turn='x'
+            set_turn('x')
+        print('iturn', turn)
+    
+    if datai!=[]:
+        connect.send_data(datai)
+        
+        
+    
     r = gameover(b)
     if r == 1:
         screen.textinput("Game over!", "X won!", winner)
@@ -199,6 +324,8 @@ def play(x, y):
 def reinitialize_game():
     
     global b
+    global player1_name
+    global player2_name
     global turn
     turtle.clear()
     
@@ -209,9 +336,9 @@ def reinitialize_game():
     player1_name = screen.textinput("Player 1 Name", "Enter Player 1's name:")
     player2_name = screen.textinput("Player 2 Name", "Enter Player 2's name:")
     
-b = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-draw(b)
-turn = 'x'
 screen.onclick(play)
+turn=get_turn()
+print(turn, 'ogoewogo')
 turtle.mainloop()
+
 ############################################################################
